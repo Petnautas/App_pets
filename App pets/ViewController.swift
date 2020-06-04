f1c5069a0c943c9f23941927954d41a0dad827cd
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2020-06-04 10:25:08 -0400

    Initial Commit

---
+++ App pets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App pets
+//
+//  Created by Vinícius de Oliveira on 04/06/20.
+//  Copyright © 2020 Vinícius de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
