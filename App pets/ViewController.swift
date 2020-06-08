b2298b18698edf7deecc866a8193d1f215550f12
Luane dos Santos Lopes <luanesantos1206@gmail.com>
2020-06-08 13:34:03 -0400

    Criação do Botão de Refresh

--- App pets/ViewController.swift
+++ App pets/ViewController.swift
@@ -13,9 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var cardImage: UIImageView!
     @IBOutlet weak var buttonInfo: UIButton!
+    @IBOutlet weak var buttonRefresh: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(red: 96/255, green: 64/255, blue: 125/255, alpha: 1)
         cardView.layer.cornerRadius = 30
         cardImage.layer.cornerRadius = 30
         
@@ -23,6 +27,10 @@ class ViewController: UIViewController {
         buttonInfo.layer.borderWidth = 1
         buttonInfo.layer.borderColor = UIColor.white.cgColor
        
+        buttonRefresh.layer.cornerRadius = 0.84*buttonInfo.bounds.size.width
+        buttonRefresh.backgroundColor = UIColor(red: 196/255, green: 177/255, blue: 214/255, alpha: 1)
+        buttonRefresh.tintColor = UIColor(red: 96/255, green: 64/255, blue: 125/255, alpha: 1)
+        
     }
 
 }
