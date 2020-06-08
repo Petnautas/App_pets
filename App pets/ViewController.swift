13b413d505259a3ee24610873c344e1f1eb2b16e
Luane dos Santos Lopes <luanesantos1206@gmail.com>
2020-06-08 09:38:58 -0400

    Criação do Botão de Informação -> buttonInfo

--- App pets/ViewController.swift
+++ App pets/ViewController.swift
@@ -12,12 +12,17 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var cardImage: UIImageView!
-    
+    @IBOutlet weak var buttonInfo: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         cardView.layer.cornerRadius = 30
         cardImage.layer.cornerRadius = 30
+        
+        buttonInfo.layer.cornerRadius = 0.5*buttonInfo.bounds.size.width
+        buttonInfo.layer.borderWidth = 1
+        buttonInfo.layer.borderColor = UIColor.white.cgColor
+       
     }
 
 }
